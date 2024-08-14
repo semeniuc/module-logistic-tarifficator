ad886bff9a62cc544fb16836c2457ac7fa3b96db
Vadim Semeniuc <semeniuc@int.pl>
2024-08-14 11:58:52 +0300

    fix: change style select

--- public/assets/js/listener/listenerTable.js
+++ public/assets/js/listener/listenerTable.js
@@ -5,34 +5,43 @@ export function handleRowSelection() {
     const tableBodies = document.querySelectorAll('table tbody');
     tableBodies.forEach(tbody => {
         tbody.addEventListener('click', function (event) {
-            // Проверяем, был ли клик по строке таблицы
             const row = event.target.closest('tr');
             if (row && !row.classList.contains('table-row-disabled')) {
-                // Если строка уже выбрана, снимаем выделение
-                if (row.classList.contains('table-row-selected')) {
-                    row.classList.remove('table-row-selected');
-                } else {
-                    // Удаляем класс выбора со всех строк
-                    tbody.querySelectorAll('tr').forEach(tr => tr.classList.remove('table-row-selected'));
+                const checkbox = row.querySelector('input[type="checkbox"]');
 
-                    // Добавляем класс выбора к выбранной строке
-                    row.classList.add('table-row-selected');
+                if (checkbox) {
+                    if (checkbox.checked) {
+                        // Удаляем класс выделения у всех строк, кроме текущей
+                        tbody.querySelectorAll('tr').forEach(tr => {
+                            const trCheckbox = tr.querySelector('input[type="checkbox"]');
+                            if (tr !== row && trCheckbox && trCheckbox.checked) {
+                                trCheckbox.checked = false;
+                                tr.classList.remove('table-row-selected');
+                            }
+                        });
 
-                    const selectedRowData = {};
+                        // Добавляем классы выделения и фона к текущей строке
+                        row.classList.add('table-row-selected');
 
-                    // Сбор данных из выбранной строки
-                    for (let i = 0; i < row.cells.length; i++) {
-                        selectedRowData[`column${i}`] = row.cells[i].innerText;
+                        const selectedRowData = {};
+
+                        // Сбор данных из выбранной строки
+                        for (let i = 0; i < row.cells.length; i++) {
+                            selectedRowData[`column${i}`] = row.cells[i].innerText;
+                        }
+
+                        // Определяем URL для отправки данных строки
+                        const tableAction = row.closest('table').getAttribute('data-action') || '/local/modules/logistic.tarifficator/api/list/select';
+
+                        // Отправляем данные выбранной строки
+                        sendAjaxRequest(tableAction, 'POST', selectedRowData, function (response) {
+                            console.log('Ответ от сервера для строки:', response);
+                            // Здесь можно выполнить дополнительные действия на основе ответа
+                        });
+                    } else {
+                        // Если чекбокс не отмечен, снимаем выделение
+                        row.classList.remove('table-row-selected');
                     }
-
-                    // Определяем URL для отправки данных строки
-                    const tableAction = row.closest('table').getAttribute('data-action') || '/local/modules/logistic.tarifficator/api/list/select';
-
-                    // Отправляем данные выбранной строки
-                    sendAjaxRequest(tableAction, 'POST', selectedRowData, function (response) {
-                        console.log('Ответ от сервера для строки:', response);
-                        // Здесь можно выполнить дополнительные действия на основе ответа
-                    });
                 }
             }
         });
