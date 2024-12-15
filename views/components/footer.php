d4b909da75367a24250f221f0de65a73d47e902f
Vadim Semeniuc <strongandluckymobile@gmail.com>
2024-12-15 12:29:17 +0200

    fix: autoremove cache

--- views/components/footer.php
+++ views/components/footer.php
@@ -3,7 +3,7 @@
 $publicDirectory = dirname($_SERVER['SCRIPT_NAME']);
 
 ?>
-<script type="module" src="<?= $publicDirectory ?>/assets/js/main.js"></script>
+<script type="module" src="<?= $publicDirectory ?>/assets/js/main.js?<?= time(); ?>"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
--- views/components/header.php
+++ views/components/header.php
@@ -15,6 +15,6 @@ global $APPLICATION;
     $APPLICATION->ShowCSS(); ?>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
-    <link href="<?= $publicDirectory ?>/assets/css/main.css" rel="stylesheet">
+    <link href="<?= $publicDirectory ?>/assets/css/main.css?<?= time(); ?>" rel="stylesheet">
 </head>
 <body>
